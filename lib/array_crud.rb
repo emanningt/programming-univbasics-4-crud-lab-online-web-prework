d139d11e37515758bad4a667d10a9b3bc9e13fe7
Eric Manning <emanningmusic@yahoo.com>
2019-09-28 04:00:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Lion","Monkey","Bear","Frog"]
-  element = array.push "bird"
+  array << "bird"
 end
 
 def add_element_to_start_of_array(array, element)
